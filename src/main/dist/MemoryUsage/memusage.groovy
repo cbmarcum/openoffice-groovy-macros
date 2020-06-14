d53e026e6cee23690d4ed1be1741259397a2d4a1
cbmarcum <carl.marcum@codebuilders.net>
2020-06-14 12:05:45 -0400

    Refs #1 - rearranged method to end of file. add guno method to replace a queryInterface. updated imports.

--- src/main/dist/MemoryUsage/memusage.groovy
+++ src/main/dist/MemoryUsage/memusage.groovy
@@ -26,7 +26,6 @@
  * derived from memusage.bsh
  */
 
-import com.sun.star.uno.UnoRuntime
 import com.sun.star.uno.AnyConverter
 import com.sun.star.uno.Type
 import com.sun.star.uno.XInterface
@@ -45,77 +44,9 @@ import com.sun.star.chart.*
 import com.sun.star.table.*
 import com.sun.star.sheet.*
 import com.sun.star.script.provider.XScriptContext
+
 // the Groovy UNO Extension
 import org.openoffice.guno.UnoExtension
-
-
-static void addData(XSpreadsheet sheet, String date, total, Long free) {
-    // set the labels
-    sheet.getCellByPosition(0, 0).setFormula("Used");
-    sheet.getCellByPosition(0, 1).setFormula("Free");
-    sheet.getCellByPosition(0, 2).setFormula("Total");
-
-    // set the values in the cells
-    sheet.getCellByPosition(1, 0).setValue(total - free);
-    sheet.getCellByPosition(1, 1).setValue(free);
-    sheet.getCellByPosition(1, 2).setValue(total);
-}
-
-static void addChart(XSpreadsheet sheet) {
-    Rectangle rect = new Rectangle()
-    rect.X = 500
-    rect.Y = 3000;
-    rect.Width = 10000
-    rect.Height = 8000
-
-    XCellRange range = (XCellRange) UnoRuntime.queryInterface(XCellRange.class, sheet)
-    XCellRange myRange = range.getCellRangeByName("A1:B2")
-
-    // XCellRangeAddressable rangeAddr = (XCellRangeAddressable)UnoRuntime.queryInterface(XCellRangeAddressable.class, myRange)
-    XCellRangeAddressable rangeAddr = myRange.guno(XCellRangeAddressable.class)
-    CellRangeAddress myAddr = rangeAddr.getRangeAddress()
-
-    CellRangeAddress[] addr = new CellRangeAddress[1]
-    addr[0] = myAddr
-
-    // XTableChartsSupplier supp = (XTableChartsSupplier)UnoRuntime.queryInterface(XTableChartsSupplier.class, sheet)
-    XTableChartsSupplier supp = sheet.guno(XTableChartsSupplier.class)
-    XTableCharts charts = supp.getCharts()
-    charts.addNewByName("Example", rect, addr, false, true)
-
-    try {
-        Thread.sleep(3000);
-    } catch (java.lang.InterruptedException e) {
-    }
-
-    // get the diagram and Change some of the properties
-    // XNameAccess chartsAccess = (XNameAccess)UnoRuntime.queryInterface(XNameAccess.class, charts)
-    XNameAccess chartsAccess = charts.guno(XNameAccess.class)
-
-    // XTableChart tchart = (XTableChart)UnoRuntime.queryInterface(XTableChart.class, chartsAccess.getByName("Example"))
-    XTableChart tchart = chartsAccess.getByName("Example").guno(XTableChart.class)
-
-    // XEmbeddedObjectSupplier eos = (XEmbeddedObjectSupplier)UnoRuntime.queryInterface(XEmbeddedObjectSupplier.class, tchart)
-
-    XEmbeddedObjectSupplier eos = tchart.guno(XEmbeddedObjectSupplier.class)
-    XComponent xifc = eos.getEmbeddedObject()
-
-    // XChartDocument xChart = (XChartDocument)UnoRuntime.queryInterface(XChartDocument.class, xifc)
-    XChartDocument xChart = xifc.guno(XChartDocument.class)
-
-    // XMultiServiceFactory xDocMSF = (XMultiServiceFactory)UnoRuntime.queryInterface(XMultiServiceFactory.class, xChart);
-    XMultiServiceFactory xDocMSF = xChart.guno(XMultiServiceFactory.class)
-
-    XInterface diagObject = xDocMSF.createInstance("com.sun.star.chart.PieDiagram")
-
-    // XDiagram xDiagram = (XDiagram)UnoRuntime.queryInterface(XDiagram.class, diagObject)
-    XDiagram xDiagram = diagObject.guno(XDiagram.class)
-    xChart.setDiagram(xDiagram)
-
-    // XPropertySet propset = (XPropertySet)UnoRuntime.queryInterface(XPropertySet.class, xChart.getTitle())
-    XPropertySet propset = xChart.getTitle().guno(XPropertySet.class)
-    propset.setPropertyValue("String", "JVM Memory Usage")
-}
 
 Runtime runtime = Runtime.getRuntime()
 Random generator = new Random()
@@ -135,3 +66,61 @@ addData(sheet, date.toString(), runtime.totalMemory(), runtime.freeMemory());
 addChart(sheet)
 
 return 0
+
+void addData(XSpreadsheet sheet, String date, total, Long free) {
+    // set the labels
+    sheet.getCellByPosition(0, 0).setFormula("Used");
+    sheet.getCellByPosition(0, 1).setFormula("Free");
+    sheet.getCellByPosition(0, 2).setFormula("Total");
+
+    // set the values in the cells
+    sheet.getCellByPosition(1, 0).setValue(total - free);
+    sheet.getCellByPosition(1, 1).setValue(free);
+    sheet.getCellByPosition(1, 2).setValue(total);
+}
+
+void addChart(XSpreadsheet sheet) {
+    Rectangle rect = new Rectangle()
+    rect.X = 500
+    rect.Y = 3000;
+    rect.Width = 10000
+    rect.Height = 8000
+
+    XCellRange range = sheet.guno(XCellRange.class)
+    XCellRange myRange = range.getCellRangeByName("A1:B2")
+
+    XCellRangeAddressable rangeAddr = myRange.guno(XCellRangeAddressable.class)
+    CellRangeAddress myAddr = rangeAddr.getRangeAddress()
+
+    CellRangeAddress[] addr = new CellRangeAddress[1]
+    addr[0] = myAddr
+
+    XTableChartsSupplier supp = sheet.guno(XTableChartsSupplier.class)
+    XTableCharts charts = supp.getCharts()
+    charts.addNewByName("Example", rect, addr, false, true)
+
+    try {
+        Thread.sleep(3000);
+    } catch (java.lang.InterruptedException e) {
+    }
+
+    // get the diagram and Change some of the properties
+    XNameAccess chartsAccess = charts.guno(XNameAccess.class)
+
+    XTableChart tchart = chartsAccess.getByName("Example").guno(XTableChart.class)
+
+    XEmbeddedObjectSupplier eos = tchart.guno(XEmbeddedObjectSupplier.class)
+    XComponent xifc = eos.getEmbeddedObject()
+
+    XChartDocument xChart = xifc.guno(XChartDocument.class)
+
+    XMultiServiceFactory xDocMSF = xChart.guno(XMultiServiceFactory.class)
+
+    XInterface diagObject = xDocMSF.createInstance("com.sun.star.chart.PieDiagram")
+
+    XDiagram xDiagram = diagObject.guno(XDiagram.class)
+    xChart.setDiagram(xDiagram)
+
+    XPropertySet propset = xChart.getTitle().guno(XPropertySet.class)
+    propset.setPropertyValue("String", "JVM Memory Usage")
+}
